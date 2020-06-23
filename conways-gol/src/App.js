fcd29ef4ac97e987cde2bef013598f0d417059bd
LeoSanchez89 <58442719+LeoSanchez89@users.noreply.github.com>
2020-06-23 19:12:14 -0400

    added bootstrap and created grid

---
+++ conways-gol/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import { Grid } from "./grid/Grid";
+import { Button } from "reactstrap";
+
+function App() {
+	return (
+		<div className="App">
+			<h1>Hello World!</h1>
+			<Grid />
+		</div>
+	);
+}
+
+export default App;
