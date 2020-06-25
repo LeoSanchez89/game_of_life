6271164bfb4697e9309f1125ea77ed95d474d0da
LeoSanchez89 <58442719+LeoSanchez89@users.noreply.github.com>
2020-06-25 19:20:15 -0400

    layout complete

--- conways-gol/src/App.js
+++ conways-gol/src/App.js
@@ -2,11 +2,33 @@ import React from "react";
 import "./App.css";
 import { Grid } from "./grid/Grid";
 
-
 function App() {
 	return (
 		<div className="App">
-			<h1>Hello World!</h1>
+			<h1>Conway's Game of Life</h1>
+			<hr />
+			<section class="main-text">
+				<div className="rules-container">
+					<h2>The Rules</h2>
+					<ul>
+						<li>Any live cell with two or three live neighbours survives</li>
+						<li>
+							Any dead cell with three live neighbours becomes a live cell
+						</li>
+						<li>All other live cells die in the next generation</li>
+					</ul>
+				</div>
+				<div className="description-container">
+					<h2>Cellular Automata</h2>
+					<p>
+						Conway's Game of Life is charactarized by a simple rule set that
+						changes the value in a cell on the grid over time, which can create
+						some very complex scenarios. Given a large enough grid and enough
+						time, anything computable can be computed in The Game of Life -
+						meaning it is "Turing Complete"
+					</p>
+				</div>
+			</section>
 			<Grid />
 		</div>
 	);
